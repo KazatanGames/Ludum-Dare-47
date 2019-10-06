58044d2ea2240c7bf659349c61249c8f2f90fabb
Daniel <daniel@kazatangames.co.uk>
2019-10-06 19:27:39 +0100

    wip

---
+++ Assets/DanCam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DanCam : MonoBehaviour
+{
+
+    protected Camera cam;
+
+    protected float wantedAspectRatio = 16f / 9f;
+
+    // Use this for initialization
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentAspectRatio = (float)Screen.width / (float)Screen.height;
+
+        float inset = 1.0f - currentAspectRatio / wantedAspectRatio;
+
+        cam.rect = new Rect(0.0f, inset / 2, 1.0f, 1.0f - inset);
+    }
+}
